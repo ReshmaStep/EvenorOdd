a4000616590eae275dc02cbedfec48b68554ce11
ReshmaStep <reshmats45@gmail.com>
2024-01-02 13:57:49 -0500

    commit swap of numbers in pom.xml

---
+++ src/main/java/swapnumbers.java
@@ -0,0 +1,19 @@
+
+public class swapnumbers {
+
+	
+		public static void main(String[] args) {
+			String x="water";
+			String y="Juice";
+			String z;
+			
+			z=x;
+			x=y;
+			y=z;
+			
+			System.out.println("x: "+x);
+			System.out.println("y: "+y);
+
+		}
+
+	}
