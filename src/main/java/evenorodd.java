f6f7028be2ba638d60ed5b49165fc0f2055ad6e1
ReshmaStep <reshmats45@gmail.com>
2024-01-02 14:27:24 -0500

    commit even or odd in pom.xml

---
+++ src/main/java/evenorodd.java
@@ -0,0 +1,15 @@
+
+public class evenorodd {
+
+	
+	public static void main(String[] args) {
+		int num=10;
+		if(num%2==0) {
+			System.out.println("number is even");
+		}
+		else {
+			System.out.println("number is odd");
+		}	
+		}
+	}
+
